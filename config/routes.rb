da90debdfc7f722b994882b3e76e06ccfad8a02a
Victoria B <viktorija.bourassa@gmail.com>
2019-03-23 19:27:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts/:id', to: 'posts#show'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: :show# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
